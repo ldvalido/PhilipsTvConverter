9c4f787639fb49d29ba2cc6c4966a833221e7453
lvalido <lvalido@mailexternos.com.ar>
2018-05-02 16:39:11 -0300

    [PhilipisTvConverter] Basic Implementation

---
+++ PhilipsTvConverter.CmdCli/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace PhilipsTvConverter.CmdCli
+{
+    public class Options
+    {
+        [Option('s', "source", Required = true, HelpText = "Input files to be processed.")]
+        public string SourcePattern { get; set; }
+
+        [Option('e',"extension",Required = false,HelpText = "Extension for the output file", Default = "mp4")]
+        public string Extension { get; set; }
+    }
+}
---
+++ PhilipsTvConverter.CmdCli/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using CommandLine;
+
+namespace PhilipsTvConverter.CmdCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args).
+                WithParsed(opts => HandleOptions(opts)).
+                WithNotParsed(errs => HandleError(errs));
+        }
+
+        public static void HandleOptions(Options opts)
+        {
+            var ffpmegPath = ConfigurationManager.AppSettings["ffmpegPath"];
+            var ffpmegArgs = ConfigurationManager.AppSettings["ffmpegArgs"];
+            var src = opts.SourcePattern;
+            var target = Path.ChangeExtension(src,opts.Extension);
+
+            var p = new Process();
+            p.StartInfo.FileName = ffpmegPath;
+            p.StartInfo.Arguments = string.Format(ffpmegArgs, src, target);
+            p.StartInfo.CreateNoWindow = false;
+            p.Start();
+            p.WaitForExit();
+        }
+
+        public static void HandleError(IEnumerable<Error> err)
+        {
+            
+        }
+    }
+}
